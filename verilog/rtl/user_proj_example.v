0ac55b84bd4ce943847c45653e6c436d86137f68
Dan Fiumara <technodan143@gmail.com>
2022-12-02 16:04:53 -0500

    12/2

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -60,20 +60,20 @@ module user_proj_example
     //carry over signals from one time register to the next
     wire sec_fine_to_sec_coarse;
     wire sec_coarse_to_min_fine;
-    wire mins_fine_to_mins_coarse;
-    wire mins_coarse_to_hours;
+    wire min_fine_to_min_coarse;
+    wire min_coarse_to_hours;
     wire hours_dummy;
 
     //signals to increment our time registers
-    wire inc_secs; 
-    wire inc_mins;
-    wire inc_hours;
+    wire inc_sec; 
+    wire inc_min;
+    wire inc_hour;
 
     //registers for storing time data
     wire [3:0] sec_fine;
     wire [11:0] sec_coarse;
-    wire [3:0] mins_fine;
-    wire [11:0] mins_coarse;
+    wire [3:0] min_fine;
+    wire [11:0] min_coarse;
     wire [11:0] hours;
 
     //outputs for clock stepdown
@@ -135,148 +135,99 @@ module user_proj_example
 
     
 
-    counter zeroto3counter(
-        .trigger(counter_trigger), //switch to IO pad later
-        .reset(our_reset),
-        .count(count)
+    button_counter zeroto3counter(
+    	.trigger(counter_trigger), //Button input counter_trigger (use clk to test)
+    	.reset(our_reset),  //our_reset
+      .counter(count)
     );
-
-    inc_demux oneto4demux(
-        .trigger(inc_demux_trigger), //pass button in here
-        .sel(count),
-        .inc_secs(inc_secs),
-        .inc_mins(inc_mins),
-        .inc_hours(inc_hours)
+  
+  inc_demux oneto4demux(
+        .trigger(inc_demux_trigger), //Button input inc_demux_trigger
+        .sel(count), //count
+        .inc_secs(inc_sec),
+        .inc_mins(inc_min),
+        .inc_hours(inc_hour)
     );
-
-    secs_fine_shift_register seconds_fine( 
-        .clk(clk_1Hz),
-        .reset(our_reset),
-        .increment(inc_secs),
-        .r_reg(sec_fine),
-        .s_out(sec_fine_to_sec_coarse)
+  
+  fine_shift_register seconds_fine( 
+       .clk(clk_1Hz),     //clk_1Hz
+       .reset(our_reset),  //our_reset
+       .increment(inc_sec),
+       .r_reg(sec_fine),
+       .s_out(sec_fine_to_sec_coarse)
     );
-
-    coarse_shift_register seconds_coarse( 
-        .clk(sec_fine_to_sec_coarse),
-        .reset(our_reset),
-        .increment(1'b0),
-        .r_reg(sec_coarse),
-        .s_out(sec_coarse_to_min_fine)
+  
+  coarse_shift_register seconds_coarse( 
+      .clk(sec_fine_to_sec_coarse),
+      .reset(our_reset),  //our_reset
+      .increment(1'b0),
+      .r_reg(sec_coarse),
+      .s_out(sec_coarse_to_min_fine)
     );
-
-    mins_fine_register minutes_fine( 
-        .clk(sec_coarse_to_min_fine),
-        .reset(our_reset),
-        .increment(inc_mins),
-        .r_reg(mins_fine), 
-        .s_out(mins_fine_to_mins_coarse)
+  
+  fine_shift_register minutes_fine( 
+      .clk(sec_coarse_to_min_fine),
+      .reset(our_reset),   //our_reset
+      .increment(inc_min),
+      .r_reg(min_fine),
+      .s_out(min_fine_to_min_coarse)
     );
-    
-    coarse_shift_register minutes_coarse( 
-        .clk(mins_fine_to_mins_coarse),
-        .reset(our_reset),
-        .increment(1'b0),
-        .r_reg(mins_coarse),
-        .s_out(mins_coarse_to_hours)
+  coarse_shift_register minutes_coarse( 
+      .clk(min_fine_to_min_coarse),
+      .reset(our_reset),   //our_reset
+      .increment(1'b0),
+      .r_reg(min_coarse),
+      .s_out(min_coarse_to_hours)
+      );
+  coarse_shift_register hrs(
+      .clk(min_coarse_to_hours),
+      .reset(our_reset),  //our_reset
+      .increment(inc_hour),
+      .r_reg(hours),
+      .s_out(hours_dummy)
+      );
+  
+  mode_processor mode_LED_processor (
+        .clk(clk_1024Hz), //clk_1024Hz
+        .mode(count),//count
+        .reset(our_reset),  //our_reset
+        .led(mode_led)
     );
-
-    coarse_shift_register hours_1 (
-        .clk(mins_coarse_to_hours),
-        .reset(our_reset),
-        .increment(inc_hours),
-        .r_reg(hours),
-        .s_out()
-    );
-
-    slowClock clock_stepdown (
+  
+  slowClock clock_stepdown (
         .clk(clk),
-        .reset(our_reset),
+        .reset(our_reset), //our_reset
         .clk_1Hz(clk_1Hz),
         .clk_1024Hz(clk_1024Hz)
     );
-
-    mode_processor mode_LED_processor (
-        .clk(clk_1024Hz),
-        .mode(count),
-        .reset(our_reset),
-        .led(mode_led)
-    );
-
-    output_mux6x6 sixbysix(
+  
+  output_mux6x6 sixbysix(
         .hours(hours),
-        .mins_coarse(mins_coarse),
+        .mins_coarse(min_coarse),
         .secs_coarse(sec_coarse),
         .clk(clk_1024Hz),
-        .reset(our_reset),
+   	    .reset(our_reset), //our_reset
         .rows(rows6x6),
         .columns(columns6x6)
     );
-
-    output_mux2x4 twobyfour(
-        .mins_fine(mins_fine), 
+  
+  output_mux2x4 twobyfour(
+        .mins_fine(min_fine), 
         .secs_fine(sec_fine),
         .clk(clk_1024Hz),
-        .reset(our_reset),
+    	  .reset(our_reset), //our_reset
         .row(row2x4),
         .columns(columns2x4)
     );
+  
 endmodule
 
-module counter #(
-    parameter BITS = 2
-)(
-    input trigger,
-    input reset,
-    output [BITS-1:0] count
-);
-    reg [BITS-1:0] count;
-
-    always @(posedge reset) begin
-        count <= 0;
-    end
-
-    always @(posedge trigger) begin
-        count <= count + 1;
-    end
 
 
-endmodule
-
-module secs_fine_shift_register
+module fine_shift_register
    #(parameter N=4)
    (
-    input wire clk, 
-    input reset, 
-    input increment,
-    output reg [N-1:0] r_reg,
-    output wire s_out
-   );
- 
-   wire [N-1:0] r_next;
- 
- 
-   always @(posedge clk or posedge reset or posedge increment)
-   begin
-      if (reset)
-         r_reg <= 4'b1000;
-      else if (r_reg[0] == 1'b1)   
-        r_reg <= 4'b0;
-      else
-         r_reg <= r_next;
-      
-	end	
- 
-	assign r_next = {1'b1, r_reg[N-1:1]};
-	assign s_out = ~r_reg[3] & ~reset;
- 
- 
-endmodule
-
-module mins_fine_register 
-   #(parameter N=4)
-   (
-    input wire clk, 
+    input wire clk,
     input wire reset, 
     input wire increment,
     output reg [N-1:0] r_reg,
@@ -286,36 +237,36 @@ module mins_fine_register
    wire [N-1:0] r_next;
    reg start;
  
-   //always @(posedge clk or posedge reset or posedge increment)
-   always @(posedge clk or posedge reset)
+  always @(posedge clk or posedge reset or posedge increment)
    begin
       if (reset)
-      begin
-         start <= 1'b1;
-         r_reg <= 4'b0;
-      end
+	begin
+        r_reg <= 4'b0000;
+     	  start <= 1'b0;
+        end
       else if (r_reg[0] == 1'b1)   
         r_reg <= 4'b0;
       else
-      begin
+        begin
          r_reg <= r_next;
-         start <= 1'b0;
-      end
+         start <= 1'b1;
+        end
       
 	end	
  
-	assign r_next = {1'b1, r_reg[N-1:1]};
-	assign s_out = ~(r_reg[3] | reset | start);
-
+  assign r_next = {1'b1, r_reg[N-1:1]};
+  assign s_out = ~r_reg[3] & ~reset & start;
+  
 endmodule
 
+
+
 module coarse_shift_register
-    #(parameter N=12)//100000000000, 
+  #(parameter N=12)//100000000000, 
    (
-    input wire clk, 
-    input reset, 
-    input increment, 
-    input sec_fine,
+    input wire clk,
+    input wire reset,
+    input wire increment,
     output reg [N-1:0] r_reg,
     output wire s_out
    );
@@ -323,117 +274,109 @@ module coarse_shift_register
    wire [N-1:0] r_next;
    reg start;
  
-   always @(posedge clk or posedge reset or posedge increment)
+  always @(posedge clk or posedge reset or posedge increment)
    begin
       if (reset)
         begin
-          start <= 1'b1;
-          r_reg <= 12'b100000000000;
+         r_reg <= 12'b100000000000;
+         start <=1'b0;
         end
       else
-      begin
-        start <= 1'b0;
-        r_reg <= r_next;
-      end
+        begin
+         r_reg <= r_next;
+         start <=1'b1;
+        end
 	end	
  
-	assign r_next = {r_reg[0], r_reg[N-1:1]};
-	assign s_out = r_reg[11] & ~reset & ~start;
- 
+  assign r_next = {r_reg[0], r_reg[N-1:1]};
+  assign s_out = r_reg[11] & ~reset & start;
  
 endmodule
 
+
+
+module button_counter 
+  #(parameter BITS = 2)
+  (
+    input wire trigger,
+    input wire reset,
+    output reg[BITS-1:0]counter
+  );
+    //reg [BITS-1:0] count;
+
+  always @(posedge reset or posedge trigger) 
+      begin
+        if(reset)
+          begin
+          counter <= 0;
+          end
+        else
+          begin
+            counter <= counter+1;
+          end
+      end
+  
+endmodule
+
+
+
 module inc_demux (
-    input trigger,
+    input wire trigger,
     input [1:0] sel,  //mode 0 = nothing, mode 1 = hours, mode 2 = mins, mode 3 = secs
+    input wire reset,
     output reg inc_secs,
     output reg inc_mins,
     output reg inc_hours
 );
-
-    always @(posedge trigger)
+  always @(posedge trigger or negedge trigger or posedge reset)
+    begin
+      if (reset)
+      begin
+        inc_secs <= 1'b0;
+        inc_mins <= 1'b0;
+        inc_hours <= 1'b0;
+      end
+      if(trigger & ~reset)
         begin
-            case(sel)
+          case(sel)
                 2'b00 : begin
-                    inc_secs = 1'b0;
-                    inc_mins = 1'b0;
-                    inc_hours = 1'b0;
+                    inc_secs <= 1'b0;
+                    inc_mins <= 1'b0;
+                    inc_hours <= 1'b0;
                 end
                 2'b01 : begin
-                    inc_secs = 1'b1;
-                    inc_mins = 1'b0;
-                    inc_hours = 1'b0;
+                    inc_secs <= 1'b1;
+                    inc_mins <= 1'b0;
+                    inc_hours <= 1'b0;
                 end
                 2'b10 : begin
-                    inc_secs = 1'b0;
-                    inc_mins = 1'b1;
-                    inc_hours = 1'b0;
+                    inc_secs <= 1'b0;
+                    inc_mins <= 1'b1;
+                    inc_hours <= 1'b0;
                 end
                 2'b11 : begin
-                    inc_secs = 1'b0;
-                    inc_mins = 1'b0;
-                    inc_hours = 1'b1;
+                    inc_secs <= 1'b0;
+                    inc_mins <= 1'b0;
+                    inc_hours <= 1'b1;
                 end
             endcase
         end
-    always@(negedge trigger)
-    begin
-        inc_secs = 1'b0;
-        inc_mins = 1'b0;
-        inc_hours = 1'b0;
+      else
+        begin
+        inc_secs <= 1'b0;
+        inc_mins <= 1'b0;
+        inc_hours <= 1'b0;
+        end
     end
+
 endmodule
 
-module slowClock(
-input clk, 
-input reset,
-output clk_1Hz, 
-output clk_1024Hz
-);
 
-
-reg clk_1Hz;
-reg clk_1024Hz;
-reg [27:0] counter; //Needs to be large enough to fit 80M
-reg [27:0] counter2;
-//parameter num_ticks = 5000000; //Number of clk ticks you want //THIS IS FOR 10MHz
-parameter num_ticks = 5; //Number of clk ticks you want
-//For 1Hz from an 80MHz clock, num_ticks=40,000,000 aka
-//half of total frequency b/c 50% duty cycle, 1 pos edge 
-//per second.
-parameter num_ticks_2=2; //4882 for 10MHz
-
-  always@(posedge clk) //posedge reset
-begin
-    if (reset)
-        begin
-            clk_1Hz <= 0;
-            clk_1024Hz <= 0;
-            counter <= 0;
-            counter2 <= 0;
-        end
-    else
-        begin
-            counter <= counter + 1;
-            counter2 <= counter2 + 1;
-          if (counter2 == num_ticks_2-1)
-              begin
-                   counter2 <= 0;
-                   clk_1024Hz <= ~clk_1024Hz;
-              end
-          if ( counter == num_ticks-1) //minus 1 to account for transistion
-                begin
-                    counter <= 0;
-                    clk_1Hz <= ~clk_1Hz;
-                end
-        end
-end
-endmodule   
 
 module mode_processor(
-  input clk, //1024Hz
+  input wire clk, //1024Hz
   input [1:0] mode,
-  input reset,
+  input wire reset,
   output reg led
 );
 
@@ -444,120 +387,182 @@ module mode_processor(
   reg clk_div4;
 
   parameter num_ticks = 256;
-always@(posedge reset or posedge clk)
-begin
-    if (reset)
-        begin
-            clk_4Hz <= 0;
-            counter <= 0;
-            clk_div1 <= 0;
-            clk_div2 <= 0;
-            clk_div4 <= 0;
-            led <= 0;
-        end
-    else
-        begin
-            counter <= counter + 1;
+  always@(posedge reset or posedge clk)
+  begin
+      if (reset)
+          begin
+              clk_4Hz <= 1'b0;
+              counter <= 11'b0;
+              clk_div1 <= 1'b0;
+          end
+      else
+          begin
+              counter <= counter + 1;
 
-          if ( counter == num_ticks-1) //minus 1 to account for transistion
-                begin
-                    counter <= 0;
-                    clk_4Hz <= ~clk_4Hz;
-                end
-        end
-end
+            if ( counter == num_ticks-1) //minus 1 to account for transistion
+                  begin
+                      counter <= 0;
+                      clk_4Hz <= ~clk_4Hz;
+                  end
+          end
+  end
 
+  always @(posedge clk_4Hz or posedge reset)
+    begin
+      if(reset)
+        begin
+          clk_div2 <= 1'b0;
+        end 
+      else
+        begin
+          clk_div2 <= ~clk_div2;
+        end
+    end
+
+  always @(posedge clk_div2 or posedge reset)
+    begin
+      if(reset)
+        begin
+          clk_div4 <= 1'b0;
+        end 
+      else
+        begin
+          clk_div4 <= ~clk_div4;
+        end
+    end
+    
   // simple ripple clock divider
-  always @(mode)
+  always @(mode)//posedge clk or posedge reset)
         begin
+         /* if(reset)
+            begin
+              led <= 1'b0;
+            end
+          else
+            begin
+            */
           case(mode)
-                0 : begin
-                    led = 0;
+                2'b00 : begin
+                    led = 1'b0;
                 end
-                1 : begin
+                2'b01 : begin
                     led = clk_div4;
                 end
-                2 : begin
+                2'b10 : begin
                     led = clk_div2;
                 end
-                3 : begin
+                2'b11 : begin
                     led = clk_4Hz;
                 end
             endcase
         end
-
-  always @(posedge clk_4Hz)
-    clk_div2 <= ~clk_div2;
-
-  always @(posedge clk_div2)
-    clk_div4 <= ~clk_div4;
-
+       // end
 
 endmodule
+
+
+module slowClock(
+  input wire clk, 
+  input wire reset,
+  output reg clk_1Hz, 
+  output reg clk_1024Hz
+  );
+
+  reg [27:0] counter; //Needs to be large enough to fit 80M
+  reg [27:0] counter2;
+  //parameter num_ticks = 5000000; //Number of clk ticks you want //THIS IS FOR 10MHz
+  parameter num_ticks = 5; //Number of clk ticks you want
+  //For 1Hz from an 80MHz clock, num_ticks=40,000,000 aka
+  //half of total frequency b/c 50% duty cycle, 1 pos edge 
+  //per second.
+  parameter num_ticks_2=1; //4882 for 10MHz
+
+  always@(posedge clk or posedge reset) //posedge reset
+  begin
+      if (reset)
+          begin
+              clk_1Hz <= 0;
+              clk_1024Hz <= 0;
+              counter <= 0;
+              counter2 <= 0;
+          end
+      else
+          begin
+              counter <= counter + 1;
+              counter2 <= counter2 + 1;
+            if (counter2 == num_ticks_2-1)
+                begin
+                     counter2 <= 0;
+                     clk_1024Hz <= ~clk_1024Hz;
+                end
+            if ( counter == num_ticks-1) //minus 1 to account for transistion
+                  begin
+                      counter <= 0;
+                      clk_1Hz <= ~clk_1Hz;
+                  end
+          end
+end
+endmodule   
+
+
 
 module output_mux6x6 (
   input [11:0] hours, 
   input [11:0] mins_coarse, 
   input [11:0] secs_coarse,
-  input clk, 
-  input reset, //clk should be suitable refresh rate for led matrix
-              // should be 6 times the target refresh for a single led
-  
+  input wire clk, 
+  input wire reset, 
   output reg [5:0] rows,
   output reg [5:0] columns
 
 );
   wire [5:0] row_next;
-  always@(posedge clk, posedge reset)
+  always@(posedge clk or posedge reset)
     begin 
-      if (reset == 1'b1)
+      if (reset)
       begin
         rows <= 6'b100000;
         columns <= 6'b000000;
       end
       else
-        rows <= row_next;
+        begin
+          rows <= row_next;
+          case(rows)
+            6'b000001 : begin //first half of hours
+              columns <= hours[11:6];
+            end
+            6'b100000 : begin
+              columns <= hours[5:0];
+            end
+            6'b010000 : begin
+              columns <= mins_coarse[11:6];
+            end
+            6'b001000 : begin
+              columns <= mins_coarse[5:0];
+            end
+            6'b000100 : begin 
+              columns <= secs_coarse[11:6];
+            end
+            6'b000010 : begin
+              columns <= secs_coarse[5:0];
+            end
+            default : begin
+              columns <= 6'b000000;
+            end
+      	  endcase
+        end
     end
   //assign r_next = {r_reg[0], r_reg[N-1:1]};
   assign row_next = {rows[0],rows[5:1]};
-
-  always@(posedge clk)
-    begin 
-      case(rows)
-        6'b000001 : begin //first half of hours
-          columns <= hours[11:6];
-        end
-        6'b100000 : begin
-          columns <= hours[5:0];
-        end
-        6'b010000 : begin
-          columns <= mins_coarse[11:6];
-        end
-        6'b001000 : begin
-          columns <= mins_coarse[5:0];
-        end
-        6'b000100 : begin 
-          columns <= secs_coarse[11:6];
-        end
-        6'b000010 : begin
-          columns <= secs_coarse[5:0];
-        end
-        default : begin
-          columns <= 6'b000000;
-        end
-      endcase
-    end
-  
-  
 endmodule
+
+
 
 module output_mux2x4 ( 
   input [3:0] mins_fine, 
   input [3:0] secs_fine,
-  input clk, 
-  input reset, //clk should be suitable refresh rate for led matrix
-              // should be 6 times the target refresh for a single led
-  
+  input wire clk, 
+  input wire reset,
   output reg [1:0] row,
   output reg [3:0] columns
 
@@ -565,30 +570,26 @@ module output_mux2x4 (
   wire [1:0] row_next;
   always@(posedge clk, posedge reset)
     begin 
-      if (reset == 1'b1)
+      if (reset)
       begin
         row <= 2'b10;
         columns <= 4'b0000;
       end
       else 
         row <= row_next;
-    end
+        case(row)
+          2'b10 : begin //first half of hours
+            columns <= mins_fine[3:0];
+          end
+          2'b01 : begin
+            columns <= secs_fine[3:0];
+          end
+          default : begin
+            columns <= 4'b0000;
+          end
+        endcase
+      end
   assign row_next = {row[0],row[1]};
-  always@(posedge clk)
-    begin 
-      case(row)
-        2'b10 : begin //first half of hours
-          columns <= mins_fine[3:0];
-        end
-        2'b01 : begin
-          columns <= secs_fine[3:0];
-        end
-        default : begin
-          columns <= 4'b0000;
-        end
-      endcase
-    end
-  
   
 endmodule
 
